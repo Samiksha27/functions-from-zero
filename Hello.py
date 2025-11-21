8ec0d714d97c357e5c89c996c76bfa3c8f23644c
Samiksha27 <samikshasharma2727@gmail.com>
2025-11-21 16:46:52 +0000

    adding file to repo

---
+++ Hello.py
@@ -0,0 +1,17 @@
+# print("Hello world")
+
+my_list = ["Apple","banana","mango"]
+my_dict = {"drink":"coffee","milk":"whole"}
+
+for item in my_list:
+    print(f"My favorite fruit is: {item}")
+
+for key, value in my_dict.items():
+    print(f"My favourite {key} is: {value}")
+
+for _, value in my_dict.items():
+    print(f"My favourite {key} is: {value}")
+# My favourite milk is: coffee
+# My favourite milk is: whole
+
+
